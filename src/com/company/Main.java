ef5d37c7a3900bc10c32c992fc88439bcd7ae9b6
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 14:34:58 +0200

    First unpaid job.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Irasyk varda: ");
+        String vardas = sc.next();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+
     }
 }
