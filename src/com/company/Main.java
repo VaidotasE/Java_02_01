f0f48ed8bc9aa1260cf307c445700a55b14d59cb
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 14:36:41 +0200

    i laikina saka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,8 +11,7 @@ public class Main {
         System.out.println(vardas);
         System.out.println(vardas);
         System.out.println(vardas);
-        System.out.println(vardas);
-        System.out.println(vardas);
+
 
     }
 }
